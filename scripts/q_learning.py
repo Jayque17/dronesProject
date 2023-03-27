1402c32b2e35b222c12d7089feebbc64809a0258
Thomas Saillard <thomas.saillard@protonmail.com>
2023-03-28 00:05:19 +0200

    first implementation of q learning

--- scripts/q_learning.py
+++ scripts/q_learning.py
@@ -14,5 +14,25 @@ def take_action(st, Q, eps, nb_actions):
 if __name__ == '__main__':
     env = ManagerEnv()
     st = env.reset()
+    Q = [[0]*env.NB_ACTIONS]*env.NB_STATES
 
-    Q = [[0]*env.NB_ACTIONS]*env.NB_ACTIONS
+    for _ in range(100):
+        # Take an action
+        action = take_action(st, Q, 0.3, env.NB_ACTIONS)
+        stp1, reward, done, _ = env.step(action)
+
+        print(st, reward, done)
+        env.render()
+        
+        # Update Q
+        action1 = take_action(stp1, Q, 0., env.NB_ACTIONS)
+        Q[st][action] = Q[st][action] + 0.1*(reward + 0.9*Q[stp1][action1] - Q[st][action])
+
+        st = stp1
+        
+        if done:
+            st = env.reset()
+
+    for i, e in enumerate(Q):
+        print(i, e)
+        
