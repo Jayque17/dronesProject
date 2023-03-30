f7dc9be1ac5940b7799901c01afd748002d2a65d
Cancrelesh <julien.leon99@gmail.com>
2023-03-31 00:33:15 +0200

    drone fly has wanted

--- scripts/testtofilepython.py
+++ scripts/testtofilepython.py
@@ -1,3 +1,3 @@
 from functionnalities import *
 
-writeActionsToPythonScript([2, 3, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 0, 0, 3, 2, 5], ".\\test.py", (2, 2), (4, 4))
+writeActionsToPythonScript([2, 3, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 0, 0, 3, 2, 5], ".\\gen_files\\test.py", (200, 200), (4, 4))
